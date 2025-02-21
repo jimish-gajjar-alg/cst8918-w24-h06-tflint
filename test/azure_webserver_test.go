8c6b7370ea590050ae1e52cbd7bb02ddff9e521a
Jimish Gajjar <gajj0008@algonquinlive.com>
2025-02-21 15:13:56 -0500

    Renamed Variables cuase error and also add LabelPrifix and subscriptionID

--- test/azure_webserver_test.go
+++ test/azure_webserver_test.go
@@ -10,7 +10,7 @@ import (
 
 // You normally want to run this under a separate "Testing" subscription
 // For lab purposes you will use your assigned subscription under the Cloud Dev/Ops program tenant
-var subscriptionID string = "<your-subscription-id>"
+var subscriptionID string = "ec1bd924-0a6a-4aa9-aa89-c980316c0449"
 
 func TestAzureLinuxVMCreation(t *testing.T) {
 	terraformOptions := &terraform.Options{
@@ -18,7 +18,7 @@ func TestAzureLinuxVMCreation(t *testing.T) {
 		TerraformDir: "../",
 		// Override the default terraform variables
 		Vars: map[string]interface{}{
-			"labelPrefix": "<your-college-username>",
+			"label_prefix": "gajj0008",
 		},
 	}
 
--- variables.tf
+++ variables.tf
@@ -1,10 +1,12 @@
 # Define config variables
-variable "labelPrefix" {
+variable "label_prefix" {
   type        = string
   description = "Your college username. This will form the beginning of various resource names."
+  default = "gajj0008"
 }
 
 variable "region" {
+  type    = string
   default = "westus3"
 }
 
